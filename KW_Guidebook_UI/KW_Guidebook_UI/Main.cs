fc0f653fafe14dcddc733cbb3728674bc3edb744
kwonnaseong <kwonnasung98@gmail.com>
2020-06-06 01:38:26 +0900

    Add Socket
    C#과 파이썬 소켓 통신을 통해 사용자가 선택한 이미지의 경로를 파이썬에게 보낸 후, 파이썬 CNN모델의 결과값을 다시 C#이 받아, 결과값에 따라 건물의 정보 출력

--- KW_Guidebook_UI/KW_Guidebook_UI/Main.cs
+++ KW_Guidebook_UI/KW_Guidebook_UI/Main.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace KW_Guidebook_UI
 {
     public partial class KW_Guidebook_Main : Form
     {
+        string cnn_Result; //파이썬에 사용자가 선택한 사진을 보낸 후, CNN모델에 적용해 건물의 이름을 파악하여 C#에 건물이름을 전송한 값을 저장한 변수 
         public KW_Guidebook_Main()
         {
             InitializeComponent();      
@@ -22,13 +25,13 @@ namespace KW_Guidebook_UI
         private void button1_Click(object sender, EventArgs e)
         {
             string image_file = string.Empty; //선택한 이미지를 저장하기 위한 변수
-
             OpenFileDialog dialog = new OpenFileDialog(); //다이얼로그 생성 
             dialog.InitialDirectory = @"C:\"; //다이얼로그 초기 위치 설정
 
             if (dialog.ShowDialog() == DialogResult.OK) //선택한 이미지 값을 변수에 넣음
             {
-                image_file = dialog.FileName;
+
+                image_file = dialog.FileName; //이미지 경로
             }
             else if (dialog.ShowDialog() == DialogResult.Cancel) //종료
             {
@@ -37,6 +40,35 @@ namespace KW_Guidebook_UI
 
             image_pictureBox.Image = Bitmap.FromFile(image_file); //Picturebox 컨트롤에 선택한 이미지 넣음
             image_pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+
+            // 소켓 생성
+            using (Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp))
+            {
+                client.Connect(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999)); // Connect 함수로 로컬(127.0.0.1)의 포트 번호 9999로 대기 중인 socket에 접속
+
+                var data = Encoding.UTF8.GetBytes(image_file); // python 파일에 이미지 경로를 UTF8타입의 byte 배열로 변환
+
+                client.Send(BitConverter.GetBytes(data.Length)); // big엔디언으로 데이터 길이를 변환하고 서버로 보낼 데이터의 길이보냄
+
+                client.Send(data); //데이터 전송
+
+                data = new byte[4];  // 데이터의 길이를 수신하기 위한 배열생성
+               
+                client.Receive(data, data.Length, SocketFlags.None); // 데이터의 길이를 수신
+
+                Array.Reverse(data);
+
+                data = new byte[BitConverter.ToInt32(data, 0)]; // 데이터의 길이만큼 byte 배열을 생성
+
+                client.Receive(data, data.Length, SocketFlags.None); // 데이터 수신
+
+                Console.WriteLine(Encoding.UTF8.GetString(data));  // 수신된 데이터를 UTF8인코딩으로 string 타입으로 변환 후 콘솔에 출력
+
+                cnn_Result=Encoding.UTF8.GetString(data); //파이썬에서 돌린 CNN 모델 결과값을 C#에 전송해, cnn_Result에 저장
+            }
+            Console.WriteLine("wait...");
+            Console.ReadLine();
         }
 
         private void image_pictureBox_Click(object sender, EventArgs e)
@@ -47,22 +79,19 @@ namespace KW_Guidebook_UI
         //이미지에 대한 정보를 출력할 버튼
         private void search_btn_Click(object sender, EventArgs e)
         {
-            // int i = getBuilding_number();
-            int i=0; //프로그램 개발을 위한 임의의 값 사용
-
             //switch문을 사용하여 사진의 건물에 따른 information 창 출력
-            switch (i) {
-                case 0: //비마관일 경우
+            switch (cnn_Result) {
+                case "bi": //비마관일 경우
                     Bima newForm1 = new Bima();
                     newForm1.Location = new Point(this.Location.X, this.Location.Y); //새로운 창이 기존의 창과 같은 위치로 띄우도록 변경
                     newForm1.Show(); //새로운 창과 기존의 창이 둘다 컨트롤 가능하도록 Show()함수 사용
                     break;
-                case 1: //새빛관일 경우
+                case "se": //새빛관일 경우
                     Saebit newForm2 = new Saebit();
                     newForm2.Location = new Point(this.Location.X, this.Location.Y); 
                     newForm2.Show(); 
                     break;
-                case 2: //참빛관일 경우
+                case "cham": //참빛관일 경우
                     Chambit newForm3 = new Chambit();
                     newForm3.Location = new Point(this.Location.X, this.Location.Y); 
                     newForm3.Show(); 
