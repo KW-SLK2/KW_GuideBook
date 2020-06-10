66c5a42fcccba70ac3d46137d613309607154400
shimshim485 <50603345+shimshim485@users.noreply.github.com>
2020-06-10 18:12:08 +0900

    건물별 엑셀연동

---
+++ Excel연동/Excel연동/Oak_class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Excel = Microsoft.Office.Interop.Excel;
+using ExcelDataReader;
+using System.IO;
+using System.Data.OleDb;
+
+namespace Excel연동
+{
+    public partial class Oak_class : Form
+    {
+        public Oak_class()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection(@"PROVIDER=MICROSOFT.ACE.OLEDB.12.0;Data Source='C:\Users\shims\Documents\카카오톡 받은 파일\옥의_강의.cell'; Extended Properties = 'Excel 12.0; HDR=yes';");
+            OleDbDataAdapter cmd = new OleDbDataAdapter("Select * from [Sheet1$]", con);
+            DataSet ds = new DataSet();
+            cmd.Fill(ds);
+            dataGridView.DataSource = ds.Tables[0];
+            con.Close();
+
+        }
+    }
+}
