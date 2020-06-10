12ba98ac04f90a722f5ee61f34ddb1321cd37dc3
shimshim485 <50603345+shimshim485@users.noreply.github.com>
2020-06-11 01:40:15 +0900

    Update bima_class.cs

--- KW_Guidebook_UI/KW_Guidebook_UI/bima_class.cs
+++ KW_Guidebook_UI/KW_Guidebook_UI/bima_class.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data.OleDb;
+
 
 
 namespace KW_Guidebook_UI
@@ -17,16 +17,6 @@ namespace KW_Guidebook_UI
         public bima_class()
         {
             InitializeComponent();
-        }
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            OleDbConnection con = new OleDbConnection(@"PROVIDER=MICROSOFT.ACE.OLEDB.12.0;Data Source='C:\Users\shims\Documents\카카오톡 받은 파일\비마_강의.cell'; Extended Properties = 'Excel 12.0; HDR=yes';");
-            OleDbDataAdapter cmd = new OleDbDataAdapter("Select * from [Sheet1$]", con);
-            DataSet ds = new DataSet();
-            cmd.Fill(ds);
-            dataGridView.DataSource = ds.Tables[0];
-            con.Close();
-
         }
 
     }
